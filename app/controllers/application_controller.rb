2a984ee5dc67d54c259f0473ed9c5b93a375c119
kannna5296 <kannnahacks5296@gmail.com>
2025-02-23 12:00:54 +0900

    適当なコントローラで遊ぶ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   def hello
-    render html: "hello, world!"
+    render html: "¡Hola, mundo!"
+  end
+  def goodbye
+    render html: "goodbye!"
   end
 end
